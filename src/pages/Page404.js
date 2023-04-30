f1f513760048e13f293bea36cc33d1e44d815d17
Szymon Tymcio <s.tymcio98@gmail.com>
2023-04-30 22:16:05 +0200

    cokolwiek

---
+++ src/pages/Page404.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Page404() {
+  return (
+    <div className="page404">
+      <img
+        className="rowerek"
+        alt="rowerek"
+        src="https://hips.hearstapps.com/hmg-prod/images/fear-crash-feature-1491929731.jpg"
+      />
+    </div>
+  );
+}
+export default Page404;
---
+++ src/pages/help/Faq.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Faq() {
+  return <div>Nie ma pomocy ahahaha</div>;
+}
